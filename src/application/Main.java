b2186b3dfa66819a73e09c7fb357bbda5738f396
vitoria <vitoriabccorrea@gmail.com>
2022-07-19 23:00:19 -0300

    main view controller first look

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,24 +1,32 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.ScrollPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			ScrollPane scrollPane = loader.load();
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+			Scene mainScene = new Scene(scrollPane);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
-		} catch(Exception e) {
+		}catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
+	} 
+
+
 	
 	public static void main(String[] args) {
 		launch(args);
