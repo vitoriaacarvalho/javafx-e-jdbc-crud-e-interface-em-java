1f02f6c17e39b0fb21a0c5fa1bf46d64687fae96
vitoria <vitoriabccorrea@gmail.com>
2022-07-22 19:46:35 -0300

    crud completed

--- src/db/DB.java
+++ src/db/DB.java
@@ -10,60 +10,61 @@ import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
-	private static Connection conn=null;
-	
+
+	private static Connection conn = null;
+
 	public static Connection getConnection() {
-		if(conn==null) {
+		if (conn == null) {
 			try {
-				Properties pros=loadProperties();
-				String url=props.getProperty("dburl");
-				conn=DriverManager.getConnection(url,props);
-			}catch (SQLException e) {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+			}
+			catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
 		return conn;
 	}
+
 	public static void closeConnection() {
-		if (conn!=null){
+		if (conn != null) {
 			try {
 				conn.close();
-			}catch(SQLException e) {
+			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
 	}
+
 	private static Properties loadProperties() {
-		try(FileInputStream fs=new FileInputStream("db.properties")){
-			Properties props=new Properties();
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties props = new Properties();
 			props.load(fs);
 			return props;
-		}catch(IOException e) {
+		}
+		catch (IOException e) {
 			throw new DbException(e.getMessage());
 		}
 	}
-	
-	
+
 	public static void closeStatement(Statement st) {
-		if(st!=null) {
+		if (st != null) {
 			try {
 				st.close();
-			}catch(SQLException e){
+			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
 	}
-	
+
 	public static void closeResultSet(ResultSet rs) {
-		if(rs!=null) {
+		if (rs != null) {
 			try {
 				rs.close();
-			}catch(SQLException e){
+			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
 	}
 }
-	
-	
-	
--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -1,18 +1,17 @@
 package model.services;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
+	private DepartmentDao dao=DaoFactory.createDepartmentDao();
 	
 	public List<Department> findAll(){
-		List<Department> list=new ArrayList<>();
-		list.add(new Department(1,"make up"));
-		list.add(new Department(2, "shoes"));
-		list.add(new Department(3, "keyboards"));
-		return list;
+		return dao.findAll();
 	}
 	
 
