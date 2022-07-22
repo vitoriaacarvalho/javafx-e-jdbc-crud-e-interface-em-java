13aeceb6cee4f039cb68e068845f9bef4f5a6688
vitoria <vitoriabccorrea@gmail.com>
2022-07-21 22:01:19 -0300

    adding intializable and adjusting tableView

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -3,11 +3,14 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import model.entities.Department;
 
 public class DepartmentListController implements Initializable{
@@ -25,8 +28,18 @@ public class DepartmentListController implements Initializable{
 		System.out.println("button works :D");
 	}
 	
+	private void initializeNodes() {
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		
+		
+		Stage stage=(Stage) Main.getMainScene().getWindow();
+		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+	}
+		
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		initializeNodes();
 	}
 	
 }
