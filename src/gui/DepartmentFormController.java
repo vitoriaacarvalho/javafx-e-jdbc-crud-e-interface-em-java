5828d013eb7eeac6e9d1134f32b5c2f161be0d4a
vitoria <vitoriabccorrea@gmail.com>
2022-07-26 00:36:58 -0300

    passing depObject to DepartmentForm

---
+++ src/gui/DepartmentFormController.java
@@ -0,0 +1,79 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import gui.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.entities.Department;
+
+public class DepartmentFormController implements Initializable {
+
+	private Department entity;
+	
+	@FXML
+	private TextField txtId;
+
+	@FXML
+	private TextField txtName;
+
+	@FXML
+	private Label labelErrorName;
+
+	@FXML
+	private Button btSave;
+
+	@FXML
+	private Button btCancel;
+
+	@FXML
+	public void onBtSaveAction() {
+		System.out.println("onBtSaveAction");
+	}
+
+	@FXML
+	public void onBtCancelAction() {
+		System.out.println("onBtCancelAction");
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+
+	public void setDepartment(Department entity) {
+		this.entity=entity;
+	}
+	
+	public void updateFormData() {
+		if (entity==null) {
+			throw new IllegalStateException("entity was null");
+		}
+		else {
+			txtId.setText(String.valueOf(entity.getId()));
+			txtName.setText(entity.getName());
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	private void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtName, 30);
+	}
+}
--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,18 +1,26 @@
 package gui;
-
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
@@ -32,8 +40,10 @@ public class DepartmentListController implements Initializable{
 	@FXML
 	private Button btNew;
 	
-	public void onBtNewAction() {
-		System.out.println("button works :D");
+	public void onBtNewAction(ActionEvent event) {
+		Stage parentStage=Utils.currentStage(event);
+		Department obj=new Department();
+		createDialogForm(obj,"/gui/DepartmentForm.fxml",parentStage);
 	}
 	
 	private void initializeNodes() {
@@ -62,6 +72,50 @@ public class DepartmentListController implements Initializable{
 		obsList=FXCollections.observableArrayList(list);
 		tableViewDepartment.setItems(obsList);	
 	}
+	
+	private void createDialogForm(Department obj, String absoluteName, Stage parentStage) {
+		try {
+			FXMLLoader loader=new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane=loader.load();
+			
+			DepartmentFormController controller=loader.getController();
+			controller.setDepartment(obj);
+			controller.updateFormData();
+			
+			
+			Stage dialogStage=new Stage();
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+		}catch(IOException e) {
+			Alerts.showAlert("IO Exception", "error creating view", e.getMessage(), AlertType.ERROR);
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 }
